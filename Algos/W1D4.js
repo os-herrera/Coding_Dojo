4b988c5d1102a642b69234c219f981df66596be1
Omar Salcido <omarsalcido46@gmail.com>
2023-02-13 10:38:23 -0700

    initial commit

---
+++ Algos/W1D4.js
@@ -0,0 +1,74 @@
+// // assigns value of 5 to x
+// var x = 5;
+    
+
+// function setX(newValue) {
+//     x = newValue;
+// }
+    
+
+// console.log(x);
+// // Output: 5
+// setX(15);
+// // x=15
+// // setX(newValue)
+
+
+
+// // setX(newValue)
+// console.log(x);
+// // Output: 15
+
+
+
+
+// var x = 5;
+    
+// function addToX(amount) {
+//     // Whwen we return something from our function,
+//     // the function ends
+
+//     //The function gives back whatever value is after the return!
+
+//     return x + amount;
+//     // 5+ -10
+//     console.log("hello there");
+// }
+    
+// console.log(x);
+// // Output 5
+
+// var result = addToX(-10);
+// // result = -5
+
+
+
+// console.log(result);
+// // Output: -5
+// console.log(x);
+// // Output: 5
+
+
+
+// function isPal(arr) {
+//     // arr is a standard name for an array variable
+//     for(var left=0; left<arr.length/2; left++) {
+//         var right = arr.length-1-left;
+//         if(arr[left] != arr[right]) {
+//             // if(1 != 1  )
+//             // if(1 != 2)
+//             return "Not a pal-indrome!";
+//         }
+//     }
+//     return "Pal-indrome!";
+// }
+    
+// var result1 = isPal( [1, 1, 2, 2, 1] );
+// // result = "Not a pal-indrome!"
+// console.log(result1);
+    
+// var result2 = isPal( [3, 2, 1, 1, 2, 3] );
+// console.log(result2);
+// // Output "Pal-indrome!"
+
+
---
+++ Algos/W1D5.js
@@ -0,0 +1,10 @@
+["a", "b", "c", "d", "e"];
+const array1 = ['a', 'b', 'c', 'd', 'e'];
+console.log('array1:', array1);
+
+const reversed = array1.reverse();
+console.log('reversed:', reversed);
+
+
+console.log('array1:', array1);
+
