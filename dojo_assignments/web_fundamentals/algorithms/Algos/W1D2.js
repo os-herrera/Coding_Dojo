8e4d229a09548db4da711386c3198e077e4b8cd9
Omar Salcido <omarsalcido46@gmail.com>
2023-02-14 16:51:41 -0700

    initial commit

---
+++ dojo_assignments/web_fundamentals/algorithms/Algos/W1D2.js
@@ -0,0 +1,33 @@
+// var a = 25;
+// a = a - 13;
+// console.log(a/2);
+// // 6
+// a = "hello";
+// console.log(a + " hello");
+// // hello hello
+
+// for(var i=0; i<10; i++) {
+//     console.log(i);
+//     i = i + 3; 
+// }
+    
+// console.log("outside of the loop " + i);
+
+// function getTotal(arrayOfNumbers) {
+    
+//     var sum = arrayOfNumbers[0];
+      
+//     for(var i=0; i<arrayOfNumbers.length; i++) {
+//     sum += arrayOfNumbers[i];
+//     console.log("the current sum is: " + sum); 
+//     }
+      
+//     console.log("the total is: " + sum);
+      
+//   }
+      
+//   getTotal([1, 3, 5]);
+//   the current sum is: 2
+// the current sum is: 5
+// the current sum is: 10
+// the total is: 10
