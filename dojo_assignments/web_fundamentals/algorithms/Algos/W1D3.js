834f3db3d056744dd773023f1feb904941d87a9b
Omar Salcido <omarsalcido46@gmail.com>
2023-02-15 14:31:35 -0700

    initial commit

---
+++ dojo_assignments/web_fundamentals/algorithms/Algos/W1D3.js
@@ -0,0 +1,38 @@
+// var isSunny = true;
+// var temperature = 45; // let's assume degrees Fahrenheit
+// var isRaining = false;
+// var whatToBring = "I should bring: ";
+    
+// if(isSunny) {
+//   whatToBring += "sunglasses, ";
+// }
+// if(temperature < 50) {
+//   whatToBring += "a coat, ";
+// }
+// if(isRaining) {
+//   whatToBring += "an umbrella, ";
+// }
+// whatToBring += "and a smile!";
+    
+// console.log(whatToBring);
+
+// for(var i=10; i>0; i--) {
+//   if(i != 2) {
+//     console.log(i);
+//   } else {
+//     console.log("ignition!");
+//   }
+// }
+    
+// console.log("liftoff!");
+
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+    
+  console.log(numbers[3])
+for(var i=0;i<numbers.length;i++){
+  console.log(i,numbers[i])
+}
+    
+console.log("there are " + countPositives + " positive values");
+
