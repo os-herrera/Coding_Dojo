fefa4def2aeeaf9ed06480bfa5eac66b8d107b7a
Omar Salcido <omarsalcido46@gmail.com>
2023-02-18 10:39:14 -0700

    commit main

--- dojo_assignments/web_fundamentals/algorithms/Algos/W1D5.js
+++ dojo_assignments/web_fundamentals/algorithms/Algos/W1D5.js
@@ -1,10 +1,26 @@
-["a", "b", "c", "d", "e"];
-const array1 = ['a', 'b', 'c', 'd', 'e'];
-console.log('array1:', array1);
+// var start = 0;
+// var end = 12;
+    
+// while(start < end) {
+//     console.log("start: " + start + ", end: " + end);
+//     start += 2;
+//     end -= 2;
+// }
 
-const reversed = array1.reverse();
-console.log('reversed:', reversed);
+// function reverse (arr){
+//   let arr2 = [];
+//   for (i=arr.length -1; i>=0; i--){
+//     let temp = arr[i];
+//     arr2.push(temp);
+//   }
+//   return arr2;
+// }
+// let x = reverse(["a","b","c","d","e"])
+// console.log(x);
 
-
-console.log('array1:', array1);
+// arr [4]='e'
+// arr [3]='d'
+// arr [2]='c'
+// arr [1]='b'
+// arr [0]='a' 
 
