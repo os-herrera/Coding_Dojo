450ceedce6ef903ea47e674afbda5d5ea34cb865
Omar Salcido <omarsalcido46@gmail.com>
2023-02-22 01:04:18 -0700

    commit main

---
+++ dojo_assignments/web_fundamentals/javascript/Dojonary/script.js
@@ -0,0 +1,13 @@
+function alertMe() {
+  alert("Ninja was liked")
+}
+
+function deleteMe() {
+  var addDefinition = document.querySelector('.add')
+  addDefinition.remove()
+}
+
+function changeText(element) {
+  element.innerText = "Logout";
+}
+
